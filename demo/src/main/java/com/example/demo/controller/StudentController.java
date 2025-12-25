0a59468f6483890eea5585b1f6bd411e780a590e
Yogeth <yogethcr@gmail.com>
2025-12-25 07:22:25 +0530

    update StudentController.java

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
 class StudentController{
-  @Autowired
   Student st=new Student();
   @GetMapping("/name")
   public String getStudentName(){
